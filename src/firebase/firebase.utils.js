288b6f51535a841a3e7517e1bfc5a4415cf0b4d8
Jordan Russel <rudzinski.a@gmail.com>
2020-12-01 18:07:05 +0100

    integrated firebase, sign in page and sign in component

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase/app';
+
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config =  {
+    apiKey: "AIzaSyDT5LG2vk7wX5x59OALVooiFOmZuRfrTa4",
+    authDomain: "store-db-e4af1.firebaseapp.com",
+    databaseURL: "https://store-db-e4af1.firebaseio.com",
+    projectId: "store-db-e4af1",
+    storageBucket: "store-db-e4af1.appspot.com",
+    messagingSenderId: "1074446279314",
+    appId: "1:1074446279314:web:86b0ecdb4296fc9ddb3c4b",
+    measurementId: "G-9HKEPPL6SD"
+  }
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+provider.setCustomParameters({ prompt: 'select_account' });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
