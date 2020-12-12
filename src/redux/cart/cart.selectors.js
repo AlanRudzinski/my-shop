2687d569474a7ee0d1ac75478c1aa4a37670f664
Jordan Russel <rudzinski.a@gmail.com>
2020-12-12 21:50:31 +0100

    checkout page, checkout item, added selectors to cart

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -5,7 +5,12 @@ const selectCart = state => state.cart;
 export const selectCartItems = createSelector(
     [selectCart],
     cart => cart.cartItems 
-    )
+);
+
+export const selectCartHidden = createSelector(
+    [selectCart],
+    cart => cart.hidden
+)
 
 export const selectCartItemsCount = createSelector(
     [selectCartItems],
@@ -15,4 +20,14 @@ export const selectCartItemsCount = createSelector(
             accumalatedQuantity + cartItem.quantity,
            0
         ),
+)
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems =>
+        cartItems.reduce(
+            (accumalatedPrice, cartItem) =>
+                accumalatedPrice + cartItem.quantity * cartItem.price,
+                0
+        )
 )
