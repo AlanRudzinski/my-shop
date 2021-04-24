a2f59ad47b547b32ba55407737dcc9063550013d
Jordan Russel <rudzinski.a@gmail.com>
2021-04-24 22:00:45 +0200

    added lazy loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import { GlobalStyles } from './global.styles';
 
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import CheckoutPage from './pages/checkout/checkout.component';
-
 import Header from './components/header/header.component';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selector';
+
+const HomePage = lazy(() => import('./pages/homepage/homepage.component'));
+const ShopPage = lazy(() => import('./pages/shop/shop.component'));
+const SignInAndSignUpPage = lazy(() => import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component'));
+const CheckoutPage = lazy(() => import('./pages/checkout/checkout.component'));
 
 const App = ({ setCurrentUser, currentUser }) => {
   useEffect(() => {
@@ -41,11 +41,12 @@ const App = ({ setCurrentUser, currentUser }) => {
         <GlobalStyles />
         <Header />
         <Switch>
+          <Suspense fallback={<div>...Loading</div>}>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/checkout' component={CheckoutPage} />
+          <Route path=  '/checkout' component={CheckoutPage} />
           <Route exact path='/signin' render={() => currentUser ? (<Redirect to='/'/>) : (<SignInAndSignUpPage />)} />
-  
+          </Suspense>
         </Switch>
       </div>
     );
